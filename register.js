66eb7741161086a0b3cbbebb483a3274af83c1be
JakeAldrinnn <custunajake21@gmail.com>
2024-03-05 16:10:05 +0800

    register

---
+++ register.js
@@ -0,0 +1,24 @@
+console.log("Hello World");
+console.error("ERROR HAPPENED!");
+console.warn("Warninggg!");
+
+//variables
+//string ="Words" , 'wowowo' , '123'
+// number = 123, 4567e, 2398.23
+//boolean = true, false
+//Object = ["apple", "banana"] ,[1,2,3,4]
+
+//type of
+//console.log(typeof(["apple"]))
+
+//DECLARATION OF VARIABLES
+
+let name = "JAKEEE ";
+    lastname = 'CUSTUNA'
+    age = 19;   
+    isProf = true;
+    hobbies = ["i love cycling","running"];
+console.log(name,age.isProf, hobbies);
+console.table(hobbies);
+console.log("My name is" + name + " " + lastname);
+console.log(`My name is ${name} ${lastname}`);
